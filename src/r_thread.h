ba389dd3f01bc40ce1bf88036be28692ce4a9884
Florian Rupprecht <floruppr@gmail.com>
2025-02-18 18:10:06 -0500

    C++17 fix deprecation warning

--- src/r_thread.h
+++ src/r_thread.h
@@ -3,7 +3,7 @@
 
 #include <future>
 #include <thread>
-
+#include <type_traits>  // for std::invoke_result/result_of
 #include "async_utils.h"
 
 namespace unigd
@@ -16,13 +16,21 @@ void ipc_close();
 void r_thread_impl(function_wrapper &&f);
 
 template <typename FunctionType>
+#if defined(__cplusplus) && __cplusplus >= 201703L
+std::future<typename std::invoke_result_t<FunctionType>> r_thread(FunctionType f)
+#else
 std::future<typename std::result_of<FunctionType()>::type> r_thread(FunctionType f)
+#endif
 {
-  typedef typename std::result_of<FunctionType()>::type result_type;
-  std::packaged_task<result_type()> task(std::move(f));
-  std::future<result_type> res(task.get_future());
-  r_thread_impl(std::move(task));
-  return res;
+#if defined(__cplusplus) && __cplusplus >= 201703L
+    typedef typename std::invoke_result_t<FunctionType> result_type;
+#else
+    typedef typename std::result_of<FunctionType()>::type result_type;
+#endif
+    std::packaged_task<result_type()> task(std::move(f));
+    std::future<result_type> res(task.get_future());
+    r_thread_impl(std::move(task));
+    return res;
 }
 
 }  // namespace async
